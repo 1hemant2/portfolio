7a4d32d36b12a95317f900ea5a6a556532fe6dca
hemant <hk443957@gmail.com>
2024-09-07 21:41:52 +0530

    portfolio updated

--- client/src/Pages/Blog.tsx
+++ client/src/Pages/Blog.tsx
@@ -1,37 +1,56 @@
-import React from 'react';
+import React from "react";
 
 const Blogs = [
-    {
-        title: 'The Role of Debouncing in Web Development: Why It Matters and How to Implement It',
-        image: 'https://miro.medium.com/v2/resize:fit:828/format:webp/0*2Nuoib86Ph8tCSoT',
-        link: 'https://medium.com/@hk443957/the-role-of-debouncing-in-web-development-why-it-matters-and-how-to-implement-it-afaf5a859010'
-    }
-]
+  {
+    title:
+      "Mastering Memory Management in JavaScript: Garbage Collection, Leaks, and Best Practices",
+    image:
+      "https://miro.medium.com/v2/resize:fit:828/format:webp/1*7q_4IKX27uP-d1_oSbajlA.jpeg",
+    link: "https://medium.com/@hk443957/mastering-memory-management-in-javascript-garbage-collection-leaks-and-best-practices-3dda7aea625a",
+  },
+  {
+    title:
+      "The Role of Debouncing in Web Development: Why It Matters and How to Implement It",
+    image:
+      "https://miro.medium.com/v2/resize:fit:828/format:webp/0*2Nuoib86Ph8tCSoT",
+    link: "https://medium.com/@hk443957/the-role-of-debouncing-in-web-development-why-it-matters-and-how-to-implement-it-afaf5a859010",
+  },
+  {
+    title: "Hack-Proof Your Node.js App: Securing MVC Routes with Express.js",
+    image:
+      "https://miro.medium.com/v2/resize:fit:828/format:webp/1*O6IoigoFWu38zWSe7MSOMg.jpeg",
+    link: "https://medium.com/@hk443957/hack-proof-your-node-js-app-securing-mvc-routes-with-express-js-5b7c405c0d69",
+  },
+];
 
 const Blog: React.FC = () => {
-    const handleClick = (url: string) => {
-        window.open(url, '_blank');
-    };
-    return (
-        <div>
-            <div className="underline text-4xl font-bold text-gray-600 flex justify-center mt-6">BLOGS</div>
-            <div className="sm:grid sm:grid-cols-3 sm:gap-3 sm:ml-[200px] sm:mr-[200px] mt-14 sm:h-[550px]  overflow-y-auto scrollbar space-y-10 sm:space-y-0 pl-6 pr-6">
-                {
-                    Blogs.map((d) => (
-                        <div className="relative h-[200px] cursor-pointer"
-                            onClick={() => handleClick(d.link)}
-                        >
-                            <img src={d.image} alt="debouncing image" className='rounded-xl' />
-                            <h3 className="absolute bottom-0 left-0 right-0 p-1 bg-black bg-opacity-30 text-white">{d.title}</h3>
-                        </div>
-                    ))
-                }
-
-            </div>
-        </div>
-
-
-    );
+  const handleClick = (url: string) => {
+    window.open(url, "_blank");
+  };
+  return (
+    <div>
+      <div className="underline text-4xl font-bold text-gray-600 flex justify-center mt-6">
+        BLOGS
+      </div>
+      <div className="sm:grid sm:grid-cols-3 sm:gap-3 sm:ml-[200px] sm:mr-[200px] mt-14 sm:h-[550px]  overflow-y-auto scrollbar space-y-10 sm:space-y-0 pl-6 pr-6 ">
+        {Blogs.map((d) => (
+          <div
+            className="relative h-[200px] cursor-pointer"
+            onClick={() => handleClick(d.link)}
+          >
+            <img
+              src={d.image}
+              alt="debouncing image"
+              className="rounded-xl h-full w-full"
+            />
+            <h3 className="absolute bottom-0 left-0 right-0 p-1 bg-black bg-opacity-50 text-white rounded-xl">
+              {d.title}
+            </h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
--- client/src/Pages/Protected.tsx
+++ client/src/Pages/Protected.tsx
@@ -1,54 +1,88 @@
-import React, { ReactNode, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { ReactNode, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface ProtectedProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 const Protected: React.FC<ProtectedProps> = ({ children }) => {
-    const navigate = useNavigate();
-    const [openMenu, setOpenMenu] = useState(false);
-    const handleNavigate = (path: string) => {
-        navigate(path);
-        setOpenMenu(!openMenu);
-    }
-    return (
-        <div className='bg-gray-200 h-screen'>
-            {/* Desktop Navigation */}
-            <nav className='hidden sm:block bg-slate-800 font-serif border-lime-100 border-b-4'>
-                <ul className='flex space-x-7 sm:pl-10 sm:ml-24 text-xl text-white items-center'>
-                    <li className='text-red-800 cursor-pointer' onClick={() => navigate("/")}>HOME</li>
-                    <li className='cursor-pointer' onClick={() => navigate("/about")}>ABOUT</li>
-                    <li className='cursor-pointer' onClick={() => navigate("/portfolio")}>PORTFOLIO</li>
-                    <li className='cursor-pointer' onClick={() => navigate("/blog")}>Blog</li>
-                    {/* <li className='cursor-pointer'>BLOG</li> */}
-                    <li className='cursor-pointer' onClick={() => navigate("/contact")}>CONTACT</li>
-                </ul>
-            </nav>
+  const navigate = useNavigate();
+  const [openMenu, setOpenMenu] = useState(false);
+  const handleNavigate = (path: string) => {
+    navigate(path);
+    setOpenMenu(!openMenu);
+  };
+  return (
+    <div className="bg-gray-200 h-screen">
+      {/* Desktop Navigation */}
+      <nav className="hidden sm:block bg-slate-800 font-serif border-lime-100 border-b-4">
+        <ul className="flex space-x-7 sm:pl-10 sm:ml-24 text-xl text-white items-center">
+          <li
+            className="text-red-800 cursor-pointer"
+            onClick={() => navigate("/")}
+          >
+            HOME
+          </li>
+          <li className="cursor-pointer" onClick={() => navigate("/about")}>
+            ABOUT
+          </li>
+          <li className="cursor-pointer" onClick={() => navigate("/portfolio")}>
+            PORTFOLIO
+          </li>
+          <li className="cursor-pointer" onClick={() => navigate("/blog")}>
+            BLOG
+          </li>
+          <li className="cursor-pointer" onClick={() => navigate("/contact")}>
+            CONTACT
+          </li>
+        </ul>
+      </nav>
 
-            {/* Mobile Navigation */}
-            <div className='block sm:hidden bg-slate-800 font-serif border-lime-100 border-b-4'>
-                <div className='flex justify-end'>
-                    <div onClick={() => setOpenMenu(!openMenu)}><i className="ri-menu-fold-3-line text-gray-400 text-3xl cursor-pointer" ></i></div>
-                </div>
-                {
-                    openMenu && (
-                        <ul className='text-white pl-4'>
-                            <li className='cursor-pointer py-2' onClick={() => handleNavigate("/")}>HOME</li>
-                            <li className='cursor-pointer py-2' onClick={() => handleNavigate("/about")}>ABOUT</li>
-                            <li className='cursor-pointer py-2' onClick={() => handleNavigate("/portfolio")}>PORTFOLIO</li>
-                            <li className='cursor-pointer py-2' onClick={() => handleNavigate("/Blog")}>Blog</li>
-                            <li className='cursor-pointer py-2' onClick={() => handleNavigate("/contact")}>CONTACT</li>
-                        </ul>
-                    )
-                }
-
-            </div>
-            <div className='bg-gray-200'>
-                {children}
-            </div>
+      {/* Mobile Navigation */}
+      <div className="block sm:hidden bg-slate-800 font-serif border-lime-100 border-b-4">
+        <div className="flex justify-end">
+          <div onClick={() => setOpenMenu(!openMenu)}>
+            <i className="ri-menu-fold-3-line text-gray-400 text-3xl cursor-pointer"></i>
+          </div>
         </div>
-    );
-}
+        {openMenu && (
+          <ul className="text-white pl-4">
+            <li
+              className="cursor-pointer py-2"
+              onClick={() => handleNavigate("/")}
+            >
+              HOME
+            </li>
+            <li
+              className="cursor-pointer py-2"
+              onClick={() => handleNavigate("/about")}
+            >
+              ABOUT
+            </li>
+            <li
+              className="cursor-pointer py-2"
+              onClick={() => handleNavigate("/portfolio")}
+            >
+              PORTFOLIO
+            </li>
+            <li
+              className="cursor-pointer py-2"
+              onClick={() => handleNavigate("/Blog")}
+            >
+              BLOG
+            </li>
+            <li
+              className="cursor-pointer py-2"
+              onClick={() => handleNavigate("/contact")}
+            >
+              CONTACT
+            </li>
+          </ul>
+        )}
+      </div>
+      <div className="bg-gray-200">{children}</div>
+    </div>
+  );
+};
 
 export default Protected;
